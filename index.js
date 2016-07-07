c7dcab900e18f52908456ec301d94ae612e040fe
luismoralesp <luismiguel.mopa@gmail.com>
2016-07-06 21:46:31 -0500

    streaming

--- index.js
+++ index.js
@@ -22,11 +22,27 @@ board.on("ready", function() {
 			led.off();
 			socket.broadcast.emit('fan-off');
 		});
+		var i = 0;
+		socket.on('data', function(message) {
+			socket.broadcast.emit('data', message);
+			console.log("data" + (++i));
+		});
 	});
 
 });
 
 var server = http.createServer(function(request, response) {
+	response.setHeader('Content-Type', 'text/html');
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	var parts = url.parse(request.url, true);
+	if (request.method == 'GET') {
+		fs.readFile('server/server.html', 'utf8', function (err, data) {
+			response.end(data);
+		});
+	}
+});
+
+var client = http.createServer(function(request, response) {
 	response.setHeader('Content-Type', 'text/html');
 	response.writeHead(200, {'Content-Type': 'text/html'});
 	var parts = url.parse(request.url, true);
@@ -41,6 +57,10 @@ http_serv.listen(1199, '0.0.0.0', function() {
 	console.log("Corriendo en el puerto ", 1199);
 });
 
-server.listen(80, '0.0.0.0', function() {
+server.listen(1198, '0.0.0.0', function() {
+	console.log("Corriendo en el puerto ", 1198);
+});
+
+client.listen(80, '0.0.0.0', function() {
 	console.log("Corriendo en el puerto ", 80);
 });
